2094c3d3664dcd65609eeb30a55905d0230a738b
kovalets-vlad <vladyslav.kovalets.shi.2023@gmail.com>
2026-04-16 14:17:07 +0300

    Refactor User model and admin interface: implement custom UserManager, update fields, and enhance admin display settings

--- users/models.py
+++ users/models.py
@@ -1,9 +1,33 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from .choices import UserRole
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is a required field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('first_name', 'Admin')
+        extra_fields.setdefault('last_name', 'Adminov')
+        
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
+    username = None  
+    
     email = models.EmailField(unique=True)
+    
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     
     role = models.CharField(
         max_length=20,
@@ -14,12 +38,14 @@ class User(AbstractUser):
     passport_number = models.CharField(max_length=20, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
     
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['first_name', 'last_name'] 
 
     def __str__(self):
-        return f"{self.email} ({self.get_role_display()})"
+        return f"{self.first_name} {self.last_name} ({self.email})"
 
     @property
     def is_staff_member(self):
